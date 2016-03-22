17abc221dfa80a3519b6ec8779587e675e5ab692
alafighting <582086295@qq.com>
2016-03-22 13:05:08 +0800

    初始化项目工程

---
+++ app/src/main/java/com/im4j/kakacache/disk/DiskCache.java
@@ -0,0 +1,9 @@
+package com.im4j.kakacache.disk;
+
+/**
+ * 磁盘缓存
+ * @author alafighting 2016-03
+ */
+public interface DiskCache {
+
+}
---
+++ app/src/main/java/com/im4j/kakacache/memory/MemoryCache.java
@@ -0,0 +1,9 @@
+package com.im4j.kakacache.memory;
+
+/**
+ * 内存缓存
+ * @author alafighting 2016-03
+ */
+public interface MemoryCache {
+
+}
